0ab68fabd993cbc74a833358a98752c48bbcbb4d
hyossid <huvsid@gmail.com>
2021-07-23 00:00:15 +0800

    Fix: test cases value changed

--- clarity/tests/weighted-equation_test.ts
+++ clarity/tests/weighted-equation_test.ts
@@ -44,38 +44,38 @@ Clarinet.test({
             types.uint(testPrice)], wallet_1.address);
         call.result.expectOk().expectUint(447258301)
 
-        call = chain.callReadOnlyFn("weighted-equation", "get-token-given-position", [types.uint(1000),
-            types.uint(1000),
+        call = chain.callReadOnlyFn("weighted-equation", "get-token-given-position", [types.uint(100000000),
+            types.uint(100000000),
             types.uint(testWeightX),
             types.uint(testWeightY),
-            types.uint(1000000),       //total-supply
-            types.uint(100),        //dX
-            types.uint(100)], wallet_1.address);
+            types.uint(100000000),       //total-supply
+            types.uint(100000000),        //dX
+            types.uint(100000000)], wallet_1.address);
         let token:any= call.result.expectOk().expectTuple()
-            token['dy'].expectUint(0);
-            token['token'].expectUint(100000);
+            token['dy'].expectUint(100000000);
+            token['token'].expectUint(100000000);
 
-        call = chain.callReadOnlyFn("weighted-equation", "get-position-given-mint", [types.uint(1000),
-            types.uint(1000),
+        call = chain.callReadOnlyFn("weighted-equation", "get-position-given-mint", [types.uint(100000000),
+            types.uint(100000000),
             types.uint(testWeightX),
             types.uint(testWeightY),
-            types.uint(2000),   //total-supply
-            types.uint(100)     //token
+            types.uint(200000000),   //total-supply
+            types.uint(10000000)     //token
             ], wallet_1.address);
         let position:any= call.result.expectOk().expectTuple()
-            position['dx'].expectUint(50);
-            position['dy'].expectUint(50);
+            position['dx'].expectUint(5000000);
+            position['dy'].expectUint(5000000);
 
-        call = chain.callReadOnlyFn("weighted-equation", "get-position-given-burn", [types.uint(1000),
-            types.uint(1000),
+        call = chain.callReadOnlyFn("weighted-equation", "get-position-given-burn", [types.uint(100000000),
+            types.uint(100000000),
             types.uint(testWeightX),
             types.uint(testWeightY),
-            types.uint(2000),   //total-supply
-            types.uint(100)     //token
+            types.uint(200000000),   //total-supply
+            types.uint(10000000)     //token
             ], wallet_1.address);
             position= call.result.expectOk().expectTuple()
-            position['dx'].expectUint(50);
-            position['dy'].expectUint(50);
+            position['dx'].expectUint(5000000);
+            position['dy'].expectUint(5000000);
     },
 });
 
