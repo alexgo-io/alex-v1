ec741fcd6d18a7c491e289d2d376e76b140162c7
fiftyeightandeight <443225+fiftyeightandeight@users.noreply.github.com>
2021-08-02 14:35:06 +0800

    use-case diagrams updated

--- diagrams/generate-diagrams.sh
+++ diagrams/generate-diagrams.sh
@@ -5,6 +5,21 @@
 class_list=''
 usecase_list='use-case-back-end use-case-arbitrageur use-case-lender use-case-borrower use-case-lp use-case-fee use-case-yield-farming use-case-borrower-repay'
 
+while getopts ":c:u:" opt; do
+  case $opt in
+    c) class_list="$OPTARG"
+    ;;
+    u) usecase_list="$OPTARG"
+    ;;
+    \?)
+    printf "***************************\n"
+    printf "* Error: Invalid argument.*\n"
+    printf "***************************\n"
+    exit 1
+    ;;
+  esac
+done
+
 for i in $class_list; do
     yuml -i $i.yuml -t class -s nofunky -f svg -o $i.svg
 done
