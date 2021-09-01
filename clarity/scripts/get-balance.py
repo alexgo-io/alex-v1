08bd96f86f52ab1ed86eb7f3b47fa3fb4a41056a
jbotwina <jimbotwina16@gmail.com>
2021-09-01 13:56:20 -0400

    renaming, adding get-nonce from scratch

--- clarity/scripts/get-balance.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-binary_balance = subprocess.check_output("curl -s \"https://stacks-node-api.regtest.stacks.co/v2/accounts/$(cat ./deploy_keychain.json | jq -r .keyInfo.address)?proof=0\" | jq -r .balance", shell=True)
-decimal_balance = int(binary_balance, 16)
-print(f"Your balance is {decimal_balance}")
