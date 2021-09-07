e1a3545f8b922fcfa2ebed34be2e0fd4d754246c
he wang <hecool108@gmail.com>
2021-09-07 20:31:28 +0800

    reset

--- init-js-tool/index.js
+++ init-js-tool/index.js
@@ -4,10 +4,10 @@ const { createFWP, createCRP,createYTP } = require('./pools')
 async function run(){
     // const {usdc, btc} = await initCoinPrice()
     //Need to call it one by one, or you'll receive 'ConflictingNonceInMempool' Error
-    // await setOpenOracle('WBTC','CoinGecko', btc);
-    // await setOpenOracle('USDA','CoinGecko', usdc);
-    // await createFWP()
-    // await createCRP('yield-usda-59760','key-usda-wbtc-59760');
-    //await createYTP('yield-usda-59760','token-wbtc', 'pool-token-usda-59760-usda')
+    //await setOpenOracle('WBTC','CoinGecko', btc);
+    //await setOpenOracle('USDA','CoinGecko', usdc);
+    //await createFWP()
+    //await createCRP('yield-usda-59760','key-usda-wbtc-59760');
+    //await createYTP('yield-usda-59760','token-wbtc', 'pool-token-usda-59760-usda');
 }
 run();
