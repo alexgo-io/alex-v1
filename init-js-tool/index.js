45fa14216a579da4269486cdd4985b70ae599fc7
he wang <hecool108@gmail.com>
2021-09-07 18:34:42 +0800

    Setup node js tool to set oracle

---
+++ init-js-tool/index.js
@@ -0,0 +1,67 @@
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+require('dotenv').config();
+const {
+  makeContractCall,
+  AnchorMode,
+  stringAsciiCV,
+  uintCV,
+  broadcastTransaction
+} = require('@stacks/transactions');
+const { StacksRegtest } = require('@stacks/network');
+const { generateWallet } = require('@stacks/wallet-sdk');
+const initCoinPrice = async () => {
+  const btc = await CoinGeckoClient.coins.fetch('bitcoin', {
+    vs_currency: 'usd',
+    from: new Date(Date.now() - 60 * 1000).getTime(),
+    to: Date.now(),
+  });
+  const usdc = await CoinGeckoClient.coins.fetch('usd-coin', {
+    vs_currency: 'usd',
+    from: new Date(Date.now() - 60 * 1000).getTime(),
+    to: Date.now(),
+  });
+  const usdcPrice = usdc.data.market_data.current_price.usd * 1e8;
+  const btcPrice = btc.data.market_data.current_price.usd * 1e8;
+  return { usdc: usdcPrice, btc: btcPrice };
+};
+const network = new StacksRegtest();
+const setOpenOracle = async (symbol, src, price) => {
+  console.log('Updating coin market price...', symbol, src, price);
+  
+  const wallet = await generateWallet({
+      secretKey:process.env.ACCOUNT_SECRET,
+      password:process.env.ACCOUNT_PWD
+  })
+  const privateKey = wallet.accounts[0].stxPrivateKey
+  
+  const txOptions = {
+    contractAddress: process.env.ACCOUNT_ADDRESS,
+    contractName: 'open-oracle',
+    functionName: 'update-price',
+    functionArgs: [
+      stringAsciiCV(symbol),
+      stringAsciiCV(src),
+      uintCV(price),
+    ],
+    senderKey: privateKey,
+    validateWithAbi: true,
+    network,
+    anchorMode: AnchorMode.Any,
+  };
+  try {
+    const transaction = await makeContractCall(txOptions);
+    const broadcastResponse = await broadcastTransaction(transaction, network);
+    const txId = broadcastResponse.txid;
+    console.log(broadcastResponse);    
+  } catch (error) {
+      console.log(error);
+  }
+  
+};
+async function run(){
+    const {usdc, btc} = await initCoinPrice()
+    // await setOpenOracle('WBTC','CoinGecko', btc);
+    // await setOpenOracle('USDA','CoinGecko', usdc);
+}
+run();
