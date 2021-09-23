da643476299803c3c6c74c1c09880ebc71290936
fiftyeightandeight <443225+fiftyeightandeight@users.noreply.github.com>
2021-09-23 18:06:51 +0800

    margin trading works

---
+++ init-js-tool/pools-ytp.js
@@ -0,0 +1,229 @@
+const {
+    getPK, network
+  } = require('./wallet');
+  const {
+    makeContractCall,
+    callReadOnlyFunction,
+    AnchorMode,
+    PostConditionMode,
+    uintCV,
+    contractPrincipalCV,
+    broadcastTransaction,
+    ClarityType
+  } = require('@stacks/transactions');
+  const { principalCV } = require('@stacks/transactions/dist/clarity/types/principalCV');
+  
+  
+  const ytpCreate = async (yiedToken, token, poolToken, multiSig, dx, dy) => {
+    console.log('[YTP] create-pool...', yiedToken, token, poolToken, multiSig, dx, dy);
+    const privateKey = await getPK();
+    const txOptions = {
+        contractAddress: process.env.ACCOUNT_ADDRESS,
+        contractName: 'yield-token-pool',
+        functionName: 'create-pool',
+        functionArgs: [
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, poolToken),
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, multiSig),            
+            uintCV(dx),
+            uintCV(dy),
+        ],
+        senderKey: privateKey,
+        validateWithAbi: true,
+        network,
+        anchorMode: AnchorMode.Any,
+        postConditionMode: PostConditionMode.Allow,
+    };
+    try {
+        const transaction = await makeContractCall(txOptions);
+        const broadcastResponse = await broadcastTransaction(transaction, network);
+        console.log(broadcastResponse);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  
+  const ytpGetPrice = async(yieldToken) => {
+    console.log('[YTP] get-price...', yieldToken);
+  
+    const options = {
+      contractAddress: process.env.ACCOUNT_ADDRESS,
+      contractName: 'yield-token-pool',
+      functionName: 'get-price',
+      functionArgs: [
+        contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yieldToken),
+      ],
+      network: network,
+      senderAddress: process.env.ACCOUNT_ADDRESS,
+    };
+    try {
+      return callReadOnlyFunction(options);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const ytpGetYield = async(yieldToken) => {
+    console.log('[YTP] get-price...', yieldToken);
+  
+    const options = {
+      contractAddress: process.env.ACCOUNT_ADDRESS,
+      contractName: 'yield-token-pool',
+      functionName: 'get-yield',
+      functionArgs: [
+        contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yieldToken),
+      ],
+      network: network,
+      senderAddress: process.env.ACCOUNT_ADDRESS,
+    };
+    try {
+      return callReadOnlyFunction(options);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const ytpSwapXforY = async (yiedToken, token, dx) => {
+    console.log('[YTP] swap-x-for-y...', yiedToken, token, dx);
+    const privateKey = await getPK();
+    const txOptions = {
+        contractAddress: process.env.ACCOUNT_ADDRESS,
+        contractName: 'yield-token-pool',
+        functionName: 'swap-x-for-y',
+        functionArgs: [
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),          
+            uintCV(dx)
+        ],
+        senderKey: privateKey,
+        validateWithAbi: true,
+        network,
+        anchorMode: AnchorMode.Any,
+        postConditionMode: PostConditionMode.Allow,
+    };
+    try {
+        const transaction = await makeContractCall(txOptions);
+        const broadcastResponse = await broadcastTransaction(transaction, network);
+        console.log(broadcastResponse);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  const ytpSwapYforX = async (yiedToken, token, dy) => {
+    console.log('[YTP] swap-y-for-x...', yiedToken, token, dy);
+    const privateKey = await getPK();
+    const txOptions = {
+        contractAddress: process.env.ACCOUNT_ADDRESS,
+        contractName: 'yield-token-pool',
+        functionName: 'swap-y-for-x',
+        functionArgs: [
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),          
+            uintCV(dy)
+        ],
+        senderKey: privateKey,
+        validateWithAbi: true,
+        network,
+        anchorMode: AnchorMode.Any,
+        postConditionMode: PostConditionMode.Allow,
+    };
+    try {
+        const transaction = await makeContractCall(txOptions);
+        const broadcastResponse = await broadcastTransaction(transaction, network);
+        console.log(broadcastResponse);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  const ytpGetXgivenY = async (yieldToken, dy) => {
+    console.log('[YTP] get-x-given-y...', yieldToken, dy);
+  
+    const options = {
+      contractAddress: process.env.ACCOUNT_ADDRESS,
+      contractName: 'yield-token-pool',
+      functionName: 'get-x-given-y',
+      functionArgs: [
+        contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yieldToken),
+        uintCV(dy)
+      ],
+      network: network,
+      senderAddress: process.env.ACCOUNT_ADDRESS,
+    };
+    try {
+      return callReadOnlyFunction(options);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const ytpGetYgivenX = async (yieldToken, dx) => {
+    console.log('[YTP] get-y-given-x...', yieldToken, dx);
+  
+    const options = {
+      contractAddress: process.env.ACCOUNT_ADDRESS,
+      contractName: 'yield-token-pool',
+      functionName: 'get-y-given-x',
+      functionArgs: [
+        contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yieldToken),
+        uintCV(dx)
+      ],
+      network: network,
+      senderAddress: process.env.ACCOUNT_ADDRESS,
+    };
+    try {
+      return callReadOnlyFunction(options);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const ytpGetPoolDetails = async (yieldToken) => {
+    console.log('[YTP] get-pool-details...', yieldToken);
+  
+    const options = {
+      contractAddress: process.env.ACCOUNT_ADDRESS,
+      contractName: 'yield-token-pool',
+      functionName: 'get-pool-details',
+      functionArgs: [
+        contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yieldToken)
+      ],
+      network: network,
+      senderAddress: process.env.ACCOUNT_ADDRESS,
+    };
+    try {
+      return callReadOnlyFunction(options);
+      printResult(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const printResult = (result)=>{
+    if(result.type === ClarityType.ResponseOk){
+        if(result.value.type == ClarityType.UInt){
+            console.log(result.value);
+        }else if(result.value.type == ClarityType.Tuple){
+            console.log('|');
+            for (const key in result.value.data) {
+                console.log('---',key,':',result.value.data[key]);
+            }
+        }
+    }
+  }
+  
+  exports.ytpCreate = ytpCreate;
+  exports.ytpGetPrice = ytpGetPrice;
+  exports.ytpGetYield = ytpGetYield;
+  exports.ytpSwapXforY = ytpSwapXforY;
+  exports.ytpSwapYforX = ytpSwapYforX;
+  exports.ytpGetYgivenX = ytpGetYgivenX;
+  exports.ytpGetXgivenY = ytpGetXgivenY;
+  exports.ytpGetPoolDetails = ytpGetPoolDetails;
+  
