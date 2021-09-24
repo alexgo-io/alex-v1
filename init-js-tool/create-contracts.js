6b6cb9eda0b6c373c6d028a49eebc87e040f031b
jbotwina <jimbotwina16@gmail.com>
2021-09-24 10:55:07 -0400

    rectified naming

--- init-js-tool/create-contracts.js
+++ init-js-tool/create-contracts.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const replace = require('replace-in-file');
 if(process.argv.length !== 5){
-        console.log("Enter the desired (1)token, (2)collateral, and then (3)expiry")
+        console.log("Enter the desired (1)collateral, (2)token, and then (3)expiry")
         process.exit(0)
     }
     
-function generateYieldTokenContract(collateral, expiry){
+function generateYieldTokenContract(token, expiry){
     let src = "../clarity/contracts/yield-token/yield-wbtc-59760.clar"
-    let dest = `./generated-contracts/yield-${collateral}-${expiry}.clar`
+    let dest = `./generated-contracts/yield-${token}-${expiry}.clar`
     fs.copyFile(src, dest, err =>{
         if (err) {
             console.log("Error found:: ", err)
@@ -16,13 +16,13 @@ function generateYieldTokenContract(collateral, expiry){
     replace.sync({
         files: dest,
         from: [/wbtc/g, /59760/g],
-        to: [collateral, expiry]
+        to: [token, expiry]
     })
 }
 
-function generatePoolTokenContract(collateral, expiry){
+function generatePoolTokenContract(token, expiry){
     let src = "../clarity/contracts/pool-token/ytp-yield-wbtc-59760-wbtc.clar"
-    let dest = `./generated-contracts/ytp-yield-${collateral}-${expiry}-${collateral}.clar`
+    let dest = `./generated-contracts/ytp-yield-${token}-${expiry}-${token}.clar`
     fs.copyFile(src, dest, err =>{
         if (err) {
             console.log("Error found:: ", err)
@@ -31,13 +31,13 @@ function generatePoolTokenContract(collateral, expiry){
     replace.sync({
         files: dest,
         from: [/wbtc/g, /59760/g],
-        to: [collateral, expiry]
+        to: [token, expiry]
     })
 }
 
-function generateKeyTokenContract(token, collateral, expiry){
+function generateKeyTokenContract(collateral, token, expiry){
     let src = "../clarity/contracts/key-token/key-wbtc-59760-usda.clar"
-    let dest = `./generated-contracts/key-${collateral}-${expiry}-${token}.clar`
+    let dest = `./generated-contracts/key-${token}-${expiry}-${collateral}.clar`
     fs.copyFile(src, dest, err =>{
         if (err) {
             console.log("Error found:: ", err)
@@ -46,13 +46,13 @@ function generateKeyTokenContract(token, collateral, expiry){
     replace.sync({
         files: dest,
         from: [/usda/g, /wbtc/g, /59760/g],
-        to: [token, collateral, expiry]
+        to: [collateral, token, expiry]
     })
 }
 
-function generateMultisigCRP(token, collateral, expiry){
+function generateMultisigCRP(collateral, token, expiry){
     let src = "../clarity/contracts/multisig/multisig-crp-wbtc-59760-usda.clar"
-    let dest = `./generated-contracts/multisig-crp-${collateral}-${expiry}-${token}.clar`
+    let dest = `./generated-contracts/multisig-crp-${token}-${expiry}-${collateral}.clar`
     fs.copyFile(src, dest, err =>{
         if (err) {
             console.log("Error found:: ", err)
@@ -61,13 +61,13 @@ function generateMultisigCRP(token, collateral, expiry){
     replace.sync({
         files: dest,
         from: [/usda/g, /wbtc/g, /59760/g],
-        to: [token, collateral, expiry]
+        to: [collateral, token, expiry]
     })
 }
 
-function generateMultisigYTPYield(collateral, expiry){
+function generateMultisigYTPYield(token, expiry){
     let src = "../clarity/contracts/multisig/multisig-ytp-yield-wbtc-59760-wbtc.clar"
-    let dest = `./generated-contracts/multisig-ytp-yield-${collateral}-${expiry}-${collateral}.clar`
+    let dest = `./generated-contracts/multisig-ytp-yield-${token}-${expiry}-${token}.clar`
     fs.copyFile(src, dest, err =>{
         if (err) {
             console.log("Error found:: ", err)
@@ -76,20 +76,20 @@ function generateMultisigYTPYield(collateral, expiry){
     replace.sync({
         files: dest,
         from: [/wbtc/g, /59760/g],
-        to: [collateral, expiry]
+        to: [token, expiry]
     })
 }
 
 
 async function run() {
-    let token = process.argv[2]
-    let collateral = process.argv[3]
+    let collateral = process.argv[2]
+    let token = process.argv[3]
     let expiry = process.argv[4]
-    generateKeyTokenContract(token, collateral, expiry)
-    generateYieldTokenContract(collateral, expiry)
-    generatePoolTokenContract(collateral, expiry)
-    generateMultisigCRP(token, collateral, expiry)
-    generateMultisigYTPYield(collateral, expiry)
+    generateKeyTokenContract(collateral, token, expiry)
+    generateYieldTokenContract(token, expiry)
+    generatePoolTokenContract(token, expiry)
+    generateMultisigCRP(collateral, token, expiry)
+    generateMultisigYTPYield(token, expiry)
 
 }
 run()
