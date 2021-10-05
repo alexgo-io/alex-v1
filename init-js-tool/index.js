52f36cfb4ec44e35d8fbe77d0fdb37d3790560e7
fiftyeightandeight <443225+fiftyeightandeight@users.noreply.github.com>
2021-10-05 08:31:30 +0800

    working

--- init-js-tool/index.js
+++ init-js-tool/index.js
@@ -497,12 +497,12 @@ async function get_pool_details_crp(){
     _list = {
         1: { token: 'token-wbtc', collateral: 'token-usda', expiry: 5760e+8 },
         2: { token: 'token-usda', collateral: 'token-wbtc', expiry: 5760e+8 },        
-        // 3: { token: 'token-wbtc', collateral: 'token-usda', expiry: 23040e+8 },
-        // 4: { token: 'token-usda', collateral: 'token-wbtc', expiry: 23040e+8 },          
-        // 5: { token: 'token-wbtc', collateral: 'token-usda', expiry: 34560e+8 }, 
-        // 6: { token: 'token-usda', collateral: 'token-wbtc', expiry: 34560e+8 },          
-        // 7: { token: 'token-wbtc', collateral: 'token-usda', expiry: 74880e+8 },            
-        // 8: { token: 'token-usda', collateral: 'token-wbtc', expiry: 74880e+8 },                                    
+        3: { token: 'token-wbtc', collateral: 'token-usda', expiry: 23040e+8 },
+        4: { token: 'token-usda', collateral: 'token-wbtc', expiry: 23040e+8 },          
+        5: { token: 'token-wbtc', collateral: 'token-usda', expiry: 34560e+8 }, 
+        6: { token: 'token-usda', collateral: 'token-wbtc', expiry: 34560e+8 },          
+        7: { token: 'token-wbtc', collateral: 'token-usda', expiry: 74880e+8 },            
+        8: { token: 'token-usda', collateral: 'token-wbtc', expiry: 74880e+8 },                                    
     }
     
     for (const key in _list){
@@ -562,20 +562,21 @@ async function reduce_position_ytp(percent){
 }
 
 _white_list = {
-    Hadan: 'STBAY5N5TRTEHHXRP4MH5H3W5FK2EXJDJWDYFA02',
-    Chiente: 'ST3N9GSEWX710RE5PSD110APZGKSD1EFMBEC7PFWK',
-    Marvin: 'SP1YMQJR0T1P52RT1VVPZZYZEQXQ5HBE6VWR36HFE',
-    James: 'STCTK0C1JAFK3JVM95TFV6EB16579WRCEYN10CTQ',
-    Jing: 'ST2Q086N22CPRA5RK306CT5T0QFG6GNMJQBY4HXZC',
-    Chan: 'ST3BQ65DRM8DMTYDD5HWMN60EYC0JFS5NC262MM33', 
-    Sidney: 'ST14YKBTNC0V2QXS3DSGFVCBJHQ7RM396511TJBTJ',
-    Liming: 'ST27WEWFJ3R3A8P20SRZHYJT1RP7GQRSB999RBN31',
-    Rachel: 'STY8YN3BJBF96FA3T916D5MFQQJ2GMKBNQW10NT5',
-    Tiger: 'ST17MVDJT37DGB5QRRS1H4HQ4MKVFKA3KAA4YGFH4',
-    Noise: 'ST290HKX9PWEQ7C3T3MFH3GZ4MXDP10F68K5GPSM2',
-    Oscar: 'ST19VTXARP3J5NFH1T69DCVJZ01CYYTWP0ME2VTX0',
-    Oscar2: 'ST2HRPEK5BC4C9CGNRT95Z85M9B9C3M99C6V7A6EZ',
-    Shawn: 'ST27TPYFGSGT3YGTEBTVMHZXD511AE6JGP15XVZDS'    
+    // Hadan: 'STBAY5N5TRTEHHXRP4MH5H3W5FK2EXJDJWDYFA02',
+    // Chiente: 'ST3N9GSEWX710RE5PSD110APZGKSD1EFMBEC7PFWK',
+    // Marvin: 'SP1YMQJR0T1P52RT1VVPZZYZEQXQ5HBE6VWR36HFE',
+    // James: 'STCTK0C1JAFK3JVM95TFV6EB16579WRCEYN10CTQ',
+    // Jing: 'ST2Q086N22CPRA5RK306CT5T0QFG6GNMJQBY4HXZC',
+    // Chan: 'ST3BQ65DRM8DMTYDD5HWMN60EYC0JFS5NC262MM33', 
+    Chan2: 'STPXVKFHHPJ9FTMQEXCM41PH1042BVG2YMM310TK',
+    // Sidney: 'ST14YKBTNC0V2QXS3DSGFVCBJHQ7RM396511TJBTJ',
+    // Liming: 'ST27WEWFJ3R3A8P20SRZHYJT1RP7GQRSB999RBN31',
+    // Rachel: 'STY8YN3BJBF96FA3T916D5MFQQJ2GMKBNQW10NT5',
+    // Tiger: 'ST17MVDJT37DGB5QRRS1H4HQ4MKVFKA3KAA4YGFH4',
+    // Noise: 'ST290HKX9PWEQ7C3T3MFH3GZ4MXDP10F68K5GPSM2',
+    // Oscar: 'ST19VTXARP3J5NFH1T69DCVJZ01CYYTWP0ME2VTX0',
+    // Oscar2: 'ST2HRPEK5BC4C9CGNRT95Z85M9B9C3M99C6V7A6EZ',
+    // Shawn: 'ST27TPYFGSGT3YGTEBTVMHZXD511AE6JGP15XVZDS'    
 }
 
 async function run(){
@@ -595,14 +596,14 @@ async function run(){
     // await test_spot_trading();
     // await test_margin_trading();
     // await get_pool_details_fwp();
-    await get_pool_details_crp();
+    // await get_pool_details_crp();
     // await get_pool_details_ytp();
     // await reduce_position_ytp(0.5e+8); // TODO: still doesn't work
-    // for(const key in _white_list){
-    //     await get_some_token(_white_list[key]);
-    //     // await burn('token-wbtc', _white_list[key], 5);
-    //     // await burn('token-usda', _white_list[key], 500000e+6);
-    // }
+    for(const key in _white_list){
+        await get_some_token(_white_list[key]);
+        // await burn('token-wbtc', _white_list[key], 5);
+        // await burn('token-usda', _white_list[key], 500000e+6);
+    }
     // await mint_some_tokens('STB2BWB0K5XZGS3FXVTG3TKS46CQVV66NAK3YVN8');
 }
 run();
