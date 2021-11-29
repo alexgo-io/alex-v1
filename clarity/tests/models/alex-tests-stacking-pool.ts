6dac8e29cc15b6e4cf00f65770c4591f2a5118f6
fiftyeightandeight <443225+fiftyeightandeight@users.noreply.github.com>
2021-11-29 20:38:18 +0800

    staked-alex added

---
+++ clarity/tests/models/alex-tests-stacking-pool.ts
@@ -0,0 +1,39 @@
+import {
+    Account,
+    Chain,
+    Clarinet,
+    Tx,
+    types,
+  } from "https://deno.land/x/clarinet@v0.14.0/index.ts";
+  
+
+  class StackingTestAgent1 {
+    chain: Chain;
+    deployer: Account;
+  
+    constructor(chain: Chain, deployer: Account) {
+      this.chain = chain;
+      this.deployer = deployer;
+    }
+  
+    addToken(user: Account, token:string, token:string, amount: number, memo: number) {
+      let block = this.chain.mineBlock([
+        Tx.contractCall("alex-vault", "flash-loan", [
+          types.principal(loanuser),
+          types.principal(token),
+          types.uint(amount),
+          types.some(types.uint(memo))
+        ], user.address),
+      ]);
+      return block.receipts[0].result;
+    }
+
+    getBalance(token: string, owner: string) {
+      return this.chain.callReadOnlyFn(token, "get-balance-fixed", [
+        types.principal(owner)
+      ], this.deployer.address);
+    }      
+
+  }
+  export { StackingTestAgent1 };
+  
