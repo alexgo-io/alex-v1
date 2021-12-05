964c78e1a0f026497435dcdd1fe732afcb1519b8
fiftyeightandeight <443225+fiftyeightandeight@users.noreply.github.com>
2021-12-05 23:05:32 +0800

    updates to wallet.js

--- init-js-tool/wallet.js
+++ init-js-tool/wallet.js
@@ -1,4 +1,7 @@
 const { generateWallet } = require('@stacks/wallet-sdk');
+const { makeSTXTokenTransfer, broadcastTransaction } =require('@stacks/transactions');
+const { wait_until_confirmation } = require('./utils');
+
 const {
     StacksRegtest,
     StacksMocknet,
@@ -28,6 +31,31 @@ const network = new StacksMocknet({
 // const network = new StacksMainnet({    
     url:'https://regtest-3.alexgo.io'
 })
+
+const BigNum = require("bn.js");
+
+const genesis_transfer = async() => {
+    console.log('genesis-transfer...');
+
+    const txOptions = {
+      recipient: process.env.DEPLOYER_ACCOUNT_ADDRESS,
+      amount: new BigNum(1000000000000000),
+      senderKey:
+        "21d43d2ae0da1d9d04cfcaac7d397a33733881081f0b2cd038062cf0ccbb752601",
+      network: network,
+    };
+    
+    try {
+        const transaction = await makeSTXTokenTransfer(txOptions);
+        const broadcastResponse = await broadcastTransaction(transaction, network);
+        console.log(broadcastResponse);
+        await wait_until_confirmation(broadcastResponse.txid)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 exports.getDeployerPK = getDeployerPK;
 exports.getUserPK = getUserPK;
 exports.network = network;
+exports.genesis_transfer = genesis_transfer;
