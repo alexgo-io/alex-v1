7afe28760cc650a3a6d191074f727bffd03b02cc
Zitao Xiong <caoer115@gmail.com>
2022-02-28 02:18:47 +0800

    feat: add update deps scripts

--- clarity/scripts/clarinet_manager.sh
+++ clarity/scripts/clarinet_manager.sh
@@ -67,14 +67,49 @@ clean() {
   rm -rf "$ROOT/${CONTRACTS_MODULE_PATH:?}"
 }
 
+update() {
+  project_name=$1
+  branch=$2
+  echo "Updating project...$project_name commit from branch head of [$branch]"
+
+  git_repo=$(cat Clarinet.json | jq -cr ".dependencies.$project_name.git")
+  contracts_path=$(cat Clarinet.json | jq -cr ".dependencies.$project_name.contracts_path")
+
+  echo "Installing from $git_repo, to $project_name"
+  mkdir -p "$TMP_GIT_DIR"/"$project_name"
+  echo git clone "$git_repo" "$TMP_GIT_DIR"/"$project_name"
+  git clone "$git_repo" "$TMP_GIT_DIR"/"$project_name"
+  pushd "$TMP_GIT_DIR"/"$project_name" > /dev/null
+  git checkout "$branch"
+  current_commit=$(git show -s --format=%H)
+  echo ------using commit------
+  git log --format=%B -n 1 $(git log -1 --pretty=format:"%h")
+  echo ------end commit------
+  popd > /dev/null
+
+  echo branch $branch has commit $current_commit
+  cat <<< $(jq ".dependencies.$project_name.commit=\"$current_commit\"" Clarinet.json) > Clarinet.json
+
+  echo "cleaning..."
+  rm -rf "$TMP_GIT_DIR"
+  echo "updated $project_name"
+}
+
 main() {
 #   check if command is valid
   if [ "$1" == "install" ]; then
     install
   elif [ "$1" == "clean" ]; then
     clean
+  elif [ "$1" == "update" ]; then
+#    check if project and branch are provided
+    if [ -z "${2:-}" ] || [ -z "${3:-}" ]; then
+      echo "Usage: clarinet_manager.sh update <project> <branch>"
+      exit 1
+    fi
+    update $2 $3
   else
-    echo "Invalid command. Use 'install' or 'clean'"
+    echo "Invalid command. Use: ./clarinet_manager.sh install | clean | update"
   fi
 }
 
